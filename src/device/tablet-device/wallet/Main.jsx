38c56132510d5d1a147bb01456130040a126eec3
dsmeshilmaring <dsmeshilmaring13@gmail.com>
2025-02-02 21:48:11 +0530

    Added tablet navigation

--- src/device/tablet-device/wallet/Main.jsx
+++ src/device/tablet-device/wallet/Main.jsx
@@ -1,9 +1,11 @@
 import Header from "../component/header";
+import TabletNav from "../component/nav";
 
 const TabletWallet = () => {
   return (
     <>
       <Header />
+      <TabletNav />
       <main className="h-full">
         <p>This is main</p>
       </main>
