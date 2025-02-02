1a1a9ebb530458295d96b7e0dca7e5f393507ea8
dsmeshilmaring <dsmeshilmaring13@gmail.com>
2025-02-02 22:45:51 +0530

    Added new feature

--- src/device/tablet-device/wallet/Main.jsx
+++ src/device/tablet-device/wallet/Main.jsx
@@ -2,6 +2,11 @@ import Header from "../component/header";
 import TabletNav from "../component/nav";
 import EyeIcon from "../../../assets/icons/eye";
 import Option from "../../../assets/icons/dot";
+import QrIcon from "../../../assets/icons/qr";
+import SendIcon from "../../../assets/icons/send";
+import UpIcon from "../../../assets/icons/up";
+import DownIcon from "../../../assets/icons/down";
+import SwapIcon from "../../../assets/icons/swap";
 
 const TabletWallet = () => {
   return (
@@ -9,8 +14,8 @@ const TabletWallet = () => {
       <Header />
       <div className="h-full w-full flex">
         <TabletNav />
-        <main className="h-full w-full bg-secondary">
-          <div className="bg-primary py-22 mt-4 rounded-[1.6rem] flex justify-around items-center p-8 m-8 gap-8">
+        <main className="h-full w-full bg-secondary px-10">
+          <div className="bg-primary py-22 mt-4 rounded-[1.6rem] flex justify-around items-center p-8 gap-8">
             <div className="">
               <div className="flex items-center gap-4 mb-8">
                 <p>Total Balance</p>
@@ -32,6 +37,31 @@ const TabletWallet = () => {
 
             <Option w={24} color={"currentColor"} />
           </div>
+
+          <ul className="grid grid-cols-2 gap-x-16 gap-y-8 mt-12">
+            <li className="flex gap-8 border p-10 items-center rounded-3xl w-full">
+              <QrIcon w={24} color={"currentColor"} />
+              <div>Recieve</div>
+            </li>
+            <li className="flex gap-8 border p-10 items-center rounded-3xl w-full">
+              <SendIcon w={24} color={"currentColor"} />
+              <div>Send</div>
+            </li>
+            <li className="flex gap-8 border p-10 items-center rounded-3xl w-full">
+              <UpIcon w={24} color={"currentColor"} />
+              <div>Buy</div>
+            </li>
+            <li className="flex gap-8 border p-10 items-center rounded-3xl w-full">
+              <DownIcon w={24} color={"currentColor"} />
+              <div>Sell</div>
+            </li>
+            <li className="flex gap-8 border p-10 items-center rounded-3xl w-full">
+              <SwapIcon w={24} color={"currentColor"} />
+              <div>Swap</div>
+            </li>
+          </ul>
+
+          <div></div>
         </main>
       </div>
       <footer></footer>
