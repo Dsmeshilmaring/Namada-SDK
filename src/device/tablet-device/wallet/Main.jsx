ed97a9dc2fd411e117136fc83bc78c7ab155d0d5
dsmeshilmaring <dsmeshilmaring13@gmail.com>
2025-02-02 19:54:21 +0530

    Added

---
+++ src/device/tablet-device/wallet/Main.jsx
@@ -0,0 +1,15 @@
+import Header from "../component/header";
+
+const TabletWallet = () => {
+  return (
+    <>
+      <Header />
+      <main className="h-full">
+        <p>This is main</p>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default TabletWallet;
